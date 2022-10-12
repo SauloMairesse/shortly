a881b1f175ba197ad4b4e622a9a761639f3ac6c3
Saulo <saulodavi2001@gmail.com>
2022-10-12 17:12:57 -0300

    sign router/controller

--- src/controllers/signController.js
+++ src/controllers/signController.js
@@ -1,19 +1,69 @@
+import db from '../database'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+dotenv.config()
 
+export async function signUp(req, res){
+    const {name, password, passwordConfirmation, email} = req.body
 
-export async function signUp(){
+    try{
 
-    try {
+        if(password !== passwordConfirmation){ 
+            throw { type: "passwords must be equal"} 
+        }
+
+        const { rows: userRegistered } = await db.query(``)
         
-    } catch (error) {
+        if( userRegistered[0] ) {
+            throw { type: "user already exist" } 
+        }
         
+        const bcryptKey = Number(process.env.BCRYPT_SALT)
+        const pwBcrypt = bcrypt.hashSync(password, bcryptKey)
+        
+        const user = {
+            name : name,
+            email: email,
+            password: pwBcrypt
+        }
+
+        await db.query(``)
+
+        return res.status(201).send({message: 'User was registered successfully'})
+        
+    }catch(err){
+        if (err.type === "passwords must be equal") { 
+            return res.sendStatus(404)
+        }
+        if (err.type === "user already exist") {
+            return res.sendStatus(404)
+        }
+
+        return res.sendStatus(500);
     }
-}
+} 
 
-export async function signIn(){
+export async function signIn(req, res){
+    const {email, password} = req.body
 
-    try {
+    try{
+
+        const {rows: userByEmail} = await db.query(``)
+
+        const pwConfirmation = bcrypt.compareSync(password, userByEmail[0].password)
+    
+        if(!userByEmail[0] || !pwConfirmation){ 
+            throw { type: "email or password wrong" } 
+        }
+    
+        const token = jwt.sign({userId: userByEmail[0].id}, process.env.JWT_KEY)
+
+        return res.status(200).send(token)
+    }catch( err ){
+        if (err.type === "email or password wrong") { 
+            return res.sendStatus(404) 
+        }
         
-    } catch (error) {
-        
+        return res.sendStatus(500);
     }
 }
---
+++ src/middlewares/tokenValidation.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export function tokenValidation(req, res, next) {
+
+    const authorization = req.headers.authorization;
+    const token = authorization?.replace("Bearer ", "").trim();
+    const JWT_KEY = process.env.JWT_KEY
+
+    if(!token) return res.sendStatus(401)
+
+    jwt.verify(token, JWT_KEY, (err, user) => {
+        if(err) return res.sendStatus(403)
+        next()
+    })
+}
