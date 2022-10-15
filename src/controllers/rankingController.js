bf1ce4686572e2afcb53b2cfa277351d17a68444
Saulo <saulodavi2001@gmail.com>
2022-10-15 12:50:50 -0300

    ranking done

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -1,9 +1,21 @@
+import db from "../database.js";
 
-export async function ranking(){
+export async function ranking(req, res){
 
     try {
-        
+        const {rows: listRank} = await db.query(`
+            SELECT users.id, users.name, COUNT(urls.url) as "linksCount", COALESCE(SUM(urls.views),0) as "visitCount"
+            FROM urls
+            LEFT JOIN users ON urls."userId" = users.id
+            GROUP BY users.id
+            ORDER BY "visitCount" DESC
+            LIMIT 10
+            `)
+
+        return res.status(200).send(listRank)
+
     } catch (error) {
-        
+
+        return res.sendStatus(500)    
     }
 }
