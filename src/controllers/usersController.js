9ee3793319e060d0b463bd8d7771143bf13d4694
Saulo <saulodavi2001@gmail.com>
2022-10-15 12:06:10 -0300

    user/me done

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,35 @@
+import db from "../database.js";
+import jwt from "jsonwebtoken";
 
-export async function userMe(){
+export async function userMe(req, res){
+    const authorization = req.headers.authorization
+    const token = authorization.replace("Bearer ", "").trim();
+    const {userId} = jwt.verify(token, process.env.JWT_KEY)
 
     try {
-        
+        const {rows: userInfo} = await db.query(`
+            SELECT users.id, users.name, SUM(urls."views") as visitCount
+            FROM users
+            LEFT JOIN urls
+            ON urls."userId" = users.id
+            WHERE users.id = $1
+            GROUP BY users.id`,
+            [userId] )
+        const {rows: urlsOfUser} = await db.query(`
+            SELECT id, "shortUrl", url, views AS "visitCount" 
+            FROM urls
+            WHERE urls."userId" = $1`,
+            [userId] )
+
+        const test = { ...userInfo[0], shortenedUrls: urlsOfUser}
+        console.log(test)
+
+        return res.status(200).send(test)
     } catch (error) {
+        if (error.type === 'not found') { 
+            return res.sendStatus(404)
+        }
         
+        return res.sendStatus(500)
     }
 }
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { userMe } from "../controllers/usersController.js";
+import { tokenValidation } from "../middlewares/tokenValidation.js";
 
 const usersRouter = Router()
+
+usersRouter.get(`/user/me`, tokenValidation,
+                            userMe)
 
 export default usersRouter
