ac1fa071bed98bf34c7abf3a00b5c24573e70910
Saulo <saulodavi2001@gmail.com>
2022-10-17 14:12:26 -0300

    try deploy

--- src/server.js
+++ src/server.js
@@ -15,6 +15,6 @@ app.use(router)
 //configuracao da porta
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Servidor em pé na porta ${port}!`);
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
